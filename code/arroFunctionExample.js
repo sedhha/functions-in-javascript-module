179bf8c0156eec6855c1cdd955bab977d7839e2f
shivam-rallyhealth <shivam-rallyhealth>
2022-07-15 16:50:58 +0530

    Added code files

---
+++ code/arroFunctionExample.js
@@ -0,0 +1,4 @@
+// 1. const <ArrowFunctionName> = (<ArgumentList>) => {<Body>;}
+// 2. const <ArrowFunctionName> = (<ArgumentList>) => <SingleLineExpression>;
+
+const multiplyNumberByTwo = (number) => number * 2;
---
+++ code/functionExpressions.js
@@ -0,0 +1,17 @@
+// 1.
+function addTwotoNumber(number) {
+  return number + 2;
+}
+
+//2.
+const addByTwo = function (number) {
+  return number + 2;
+};
+//3.
+let addByTwoUsingLet = function (number) {
+  return number + 2;
+};
+//4.
+var addByTwoUsingVar = function (number) {
+  return number + 2;
+};
